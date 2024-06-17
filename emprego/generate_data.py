c13ef069cd1269313b078361a5969daa4aaaf55a
Victor-Buendia <victorbuendia03@gmail.com>
2024-06-16 21:34:00 -0300

    🚚 Rename thread to process

--- emprego/generate_data.py
+++ emprego/generate_data.py
@@ -10,23 +10,24 @@ log = get_logger('generate_data')
 processes = int(os.environ.get('PROCESSES_NO'))
 raw_path = os.environ.get('RAW_PATH')
 
-def generate_data(thread_id: int, model: BaseModel, record_amount: int) -> None:
+def generate_data(process_id: int, model: BaseModel, record_amount: int) -> None:
     try:
-        faker.Faker.seed(thread_id)
+        faker.Faker.seed(process_id)
         model_name = model().__class__.__name__
-        log.info(f'Generating data for {model_name} in thread {thread_id}...')
+        log.debug(f'Generating data for {model_name} in process {process_id}...')
 
         data = [model().__dict__ for _ in range(record_amount)]
-        log.info(f'Data successfully generated for {model_name} in thread {thread_id}...')
+        log.debug(f'Data successfully generated for {model_name} in process {process_id}...')
 
-        log.info(f'Saving data for {model_name} in thread {thread_id}...')
+        log.debug(f'Saving data for {model_name} in process {process_id}...')
 
-        with open(raw_path.format(model_name.lower(), model_name.lower(), thread_id), 'w') as file:
-            path = raw_path.format(model_name.lower(), model_name.lower(), thread_id)
-            log.info(f'Saving data for for {model_name} in thread {thread_id} at {path}...')
+        with open(raw_path.format(model_name.lower(), model_name.lower(), process_id), 'w') as file:
+            path = raw_path.format(model_name.lower(), model_name.lower(), process_id)
+            log.debug(f'Saving data for for {model_name} in process {process_id} at {path}...')
 
             file.write(json.dumps(data, indent = 4))
-            log.info(f'Data successfully saved for {model_name} in thread {thread_id} at {path}...')
+            log.debug(f'Data successfully saved for {model_name} in process {process_id} at {path}...')
+            log.info(f"Generated {record_amount} records for {model_name} in process {process_id}")
     except Exception as e:
         log.error(e)
 
@@ -34,12 +35,11 @@ if __name__ == '__main__':
 
     pool = multiprocessing.Pool()
     for model, records in models:
-        [
+        for i in range(processes):
             pool.apply_async(
-            func = generate_data,
-            args = (i, model, records),
-            ) for i in range(processes)
-        ]
+                func = generate_data,
+                args = (i, model, records),
+            )
     pool.close()
     pool.join()
-    log.info("Data generation completed")
+    log.debug("Data generation completed")
